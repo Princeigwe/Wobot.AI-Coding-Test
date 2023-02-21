253897944f17e6aa8efe34a5cc283408e441a02a
Prince Igwe <igwep297@gmail.com>
2023-02-21 17:59:51 +0100

    performed user registration

--- wobot_ai/settings.py
+++ wobot_ai/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     #3rd party packages
     'rest_framework',
+    'djoser',
 
     #local apps
     'account.apps.AccountConfig',
@@ -146,5 +147,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
+
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('JWT',),
 }
--- wobot_ai/urls.py
+++ wobot_ai/urls.py
@@ -18,5 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/todo/', include('todo.urls'))
+    path('api/todo/', include('todo.urls')),
+
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
