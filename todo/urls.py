45aadb049a15a21aefb4c09b2295f0b1a623a573
Prince Igwe <igwep297@gmail.com>
2023-02-21 20:00:00 +0100

    added feature that fetches tasks meant for the logged in user

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,9 @@
-from .views import TodoListCreate, TodoItem
+from .views import TodoCreate, TodoItem, TodoList
 from django.urls import path
 
 urlpatterns = [
-    path('', TodoListCreate.as_view()),
+    path('', TodoCreate.as_view()),
+    path('list/', TodoList.as_view()),
     # This is a url pattern that will match any url that has an integer in it for a the todo task.
     path('<int:pk>/', TodoItem.as_view())
 ]
--- todo/views.py
+++ todo/views.py
@@ -8,13 +8,23 @@ from .permissions import IsOwner
 
 
 # the class responsible for creating and listing todo tasks
-class TodoListCreate(generics.ListCreateAPIView):
+class TodoCreate(generics.CreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
     # linking the logged in user to the created task
     def perform_create(self, serializer): 
         serializer.save(owner=self.request.user)
+
+
+class TodoList(generics.ListAPIView):
+    serializer_class = TodoSerializer
+    # permission_classes = (IsOwner,)
+
+    # filter tasks related to a specific user
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(owner=user)
 
 # the class responsible for reading, updating and deleting a todo task
 class TodoItem(generics.RetrieveUpdateDestroyAPIView):
