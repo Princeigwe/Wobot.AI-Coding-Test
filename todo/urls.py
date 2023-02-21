0949ab4ee6fa1706b7cc814cf210563814f5a14f
Prince Igwe <igwep297@gmail.com>
2023-02-21 14:08:30 +0100

    added little commments

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', TodoListCreate.as_view()),
+    # This is a url pattern that will match any url that has an integer in it for a the todo task.
     path('<int:pk>/', TodoItem.as_view())
 ]
--- todo/views.py
+++ todo/views.py
@@ -6,11 +6,12 @@ from .serializers import TodoSerializer
 # Create your views here.
 
 
+# the class responsible for creating and listing todo tasks
 class TodoListCreate(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
-
+# the class responsible for reading, updating and deleting a todo task
 class TodoItem(generics.RetrieveUpdateDestroyAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
